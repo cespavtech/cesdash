37f764db58079b61174e5e7d0d47c71e7ec30b96
Abdirahim Abdi <info@cestropavia.co.ke>
2021-11-18 23:21:06 +0300

    Add files via upload
    Template layout finished now

---
+++ pages/deps/headers.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);  
+
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="assets/images/defaults/logo.png">
+	<title>CesDash - Bootsrap5 Dashboard Template | Cestro Pavia</title>
+	<!-- Core CSS Frameworks -->
+	<!-- Google Icons -->
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap5/css/bootstrap.css">
+	<!-- Material Preloader -->
+	<link rel="stylesheet" type="text/css" href="plugins/preloader/mpl.css">
+	<!-- AOS -->
+	<link rel="stylesheet" type="text/css" href="plugins/aos/aos.css">
+
+	<!-- Template CSS -->
+	<!-- This file customizes the bootstrap css styling -->
+	<!-- Every customization made here is excluded from bootstrap.css file! -->
+	<link rel="stylesheet" type="text/css" href="style/css/template.css">
+	<!-- Theme CSS -->
+	<!-- All coloring and gradients are here! -->
+	<link rel="stylesheet" type="text/css" href="style/css/theme-dark.css">
+	<link rel="stylesheet" type="text/css" href="style/css/theme-light.css">
+</head>
+<!--
+PRO-TIP: 
+./Change general theme by altering theme-light|theme-dark 
+./Change sidebar/navbar/ bordering by altering classes;-
+sidebar-bordered|navbar-bordered
+-->
+<body class="theme-light sidebar-bordered navbar-bordered">
